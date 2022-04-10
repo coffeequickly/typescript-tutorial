9db7ea7d4ee056f9b35a221a9c8ebaa142211e26
coffeequickly <jangwonseok.js@gmail.com>
2022-04-11 00:11:00 +0900

    ~10강(타입스크립트를 써야하는 이유)

--- why-ts/app.js
+++ why-ts/app.js
@@ -16,6 +16,8 @@ function startApp() {
       console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+
+      username.innerHTML = user.name;
     })
     .catch(function (error) {
       console.log(error);
